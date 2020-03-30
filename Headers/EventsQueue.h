27fae76faaaf5f5f0404bf4ed3b5f271c446709b
shaffei <s-mohamed_chaffei@zewailcity.edu.eg>
2020-03-30 12:07:50 +0200

    Important bug fixes 1- Changed the data structure used from dynamic array to doubly linked list 2- New data structure fixes the bugs in the previous data sructure

--- Headers/EventsQueue.h
+++ Headers/EventsQueue.h
@@ -1,6 +1,7 @@
 //
 // Created by shaffei on 3/24/20.
-//
+//Implements a queue of events
+////original source: http://rosettacode.org/wiki/Queue/Definition#C
 
 #ifndef OS_STARTER_CODE_EVENTS_queue_H
 #define OS_STARTER_CODE_EVENTS_queue_H
@@ -8,57 +9,58 @@
 #include "EventStruct.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-typedef Event *EVENT_DATA; /* type of EVENT_DATA to store in event_queue */
-typedef struct {
-    EVENT_DATA *buf;
-    size_t head, tail, alloc;
-} event_queue_t_event, *event_queue;
+typedef struct e_node_t e_node_t, *e_node, *event_queue;
+struct e_node_t {
+    Event *val;
+    e_node prev, next;
+};
+typedef Event *EVENT_DATA;
+
+#define HEAD_E(q) q->prev
+#define TAIL_E(q) q->next
 
 event_queue NewEventQueue() {
-    event_queue q = malloc(sizeof(event_queue_t_event));
-    q->buf = malloc(sizeof(EVENT_DATA) * (q->alloc = 4));
-    q->head = q->tail = 0;
+    e_node q = (e_node) malloc(sizeof(node_t));
+    q->next = q->prev = 0;
     return q;
 }
 
 int EventQueueEmpty(event_queue q) {
-    return q->tail == q->head;
+    return !HEAD_E(q);
 }
 
-void EventQueueEnqueue(event_queue q, EVENT_DATA n) {
-    if (q->tail >= q->alloc) q->tail = 0;
-    q->buf[q->tail++] = n;
-
-    // Fixed bug where it failed to resize
-    if (q->tail == q->alloc) {  /* needs more room */
-        q->buf = realloc(q->buf, sizeof(EVENT_DATA) * q->alloc * 2);
-        if (q->head) {
-            memcpy(q->buf + q->head + q->alloc, q->buf + q->head,
-                   sizeof(EVENT_DATA) * (q->alloc - q->head));
-            q->head += q->alloc;
-        } else
-            q->tail = q->alloc;
-        q->alloc *= 2;
-    }
+void EventQueueEnqueue(event_queue q, EVENT_DATA val) {
+    e_node nd = (e_node) malloc(sizeof(node_t));
+    nd->val = val;
+    if (!HEAD_E(q))
+        HEAD_E(q) = nd;
+    nd->prev = TAIL_E(q);
+    if (nd->prev)
+        nd->prev->next = nd;
+    TAIL_E(q) = nd;
+    nd->next = 0;
 }
 
-int EventQueueDequeue(event_queue q, EVENT_DATA *n) {
-    if (q->head == q->tail) return 0;
-    *n = q->buf[q->head++];
-    if (q->head >= q->alloc) { /* reduce allocated storage no longer needed */
-        q->head = 0;
-        if (q->alloc >= 512 && q->tail < q->alloc / 2)
-            q->buf = realloc(q->buf, sizeof(EVENT_DATA) * (q->alloc /= 2));
-    }
+int EventQueueDequeue(event_queue q, EVENT_DATA *val) {
+    e_node tmp = HEAD_E(q);
+    if (!tmp)
+        return 0;
+    *val = tmp->val;
+
+    HEAD_E(q) = tmp->next;
+    if (TAIL_E(q) == tmp)
+        TAIL_E(q) = 0;
+    free(tmp);
+
     return 1;
 }
 
-int EventQueuePeek(event_queue q, EVENT_DATA *n) {
-    if (q->head == q->tail)
+int EventQueuePeek(event_queue q, EVENT_DATA *val) {
+    e_node tmp = HEAD_E(q);
+    if (!tmp)
         return 0;
-    *n = q->buf[q->head];
+    *val = tmp->val;
     return 1;
 }
 
--- Headers/ProcessQueue.h
+++ Headers/ProcessQueue.h
@@ -6,60 +6,61 @@
 #ifndef OS_STARTER_CODE_PROCESSQUEUE_H
 #define OS_STARTER_CODE_PROCESSQUEUE_H
 
-#include "ProcessStruct.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include "ProcessStruct.h"
 
-typedef Process *DATA; /* type of data to store in queue */
-typedef struct {
-    DATA *buf;
-    size_t head, tail, alloc;
-} queue_t, *queue;
+typedef struct node_t_q node_t_q, *node, *queue;
+struct node_t_q {
+    Process *val;
+    node prev, next;
+};
+typedef Process *DATA;
+
+#define HEAD(q) q->prev
+#define TAIL(q) q->next
 
 queue NewProcQueue() {
-    queue q = malloc(sizeof(queue_t));
-    q->buf = malloc(sizeof(DATA) * (q->alloc = 4));
-    q->head = q->tail = 0;
+    node q = (node) malloc(sizeof(node_t_q));
+    q->next = q->prev = 0;
     return q;
 }
 
 int ProcQueueEmpty(queue q) {
-    return q->tail == q->head;
+    return !HEAD(q);
 }
 
 void ProcEnqueue(queue q, DATA n) {
-    if (q->tail >= q->alloc) q->tail = 0;
-    q->buf[q->tail++] = n;
-
-    // Fixed bug where it failed to resize
-    if (q->tail == q->alloc) {  /* needs more room */
-        q->buf = realloc(q->buf, sizeof(DATA) * q->alloc * 2);
-        if (q->head) {
-            memcpy(q->buf + q->head + q->alloc, q->buf + q->head,
-                   sizeof(DATA) * (q->alloc - q->head));
-            q->head += q->alloc;
-        } else
-            q->tail = q->alloc;
-        q->alloc *= 2;
-    }
+    node nd = (node) malloc(sizeof(node_t_q));
+    nd->val = n;
+    if (!HEAD(q))
+        HEAD(q) = nd;
+    nd->prev = TAIL(q);
+    if (nd->prev)
+        nd->prev->next = nd;
+    TAIL(q) = nd;
+    nd->next = 0;
 }
 
-int ProcDequeue(queue q, DATA *n) {
-    if (q->head == q->tail) return 0;
-    *n = q->buf[q->head++];
-    if (q->head >= q->alloc) { /* reduce allocated storage no longer needed */
-        q->head = 0;
-        if (q->alloc >= 512 && q->tail < q->alloc / 2)
-            q->buf = realloc(q->buf, sizeof(DATA) * (q->alloc /= 2));
-    }
+int ProcDequeue(queue q, DATA *val) {
+    node tmp = HEAD(q);
+    if (!tmp)
+        return 0;
+    *val = tmp->val;
+
+    HEAD(q) = tmp->next;
+    if (TAIL(q) == tmp)
+        TAIL(q) = 0;
+    free(tmp);
+
     return 1;
 }
 
-int ProcPeek(queue q, DATA *n) {
-    if (q->head == q->tail)
+int ProcPeek(queue q, DATA *val) {
+    node tmp = HEAD(q);
+    if (!tmp)
         return 0;
-    *n = q->buf[q->head];
+    *val = tmp->val;
     return 1;
 }
 
