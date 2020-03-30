b8078624ac3127030f4ac1432eaabf0b4d3b340e
shaffei <s-mohamed_chaffei@zewailcity.edu.eg>
2020-03-30 12:06:58 +0200

    Added PrintEvent() function

--- Headers/EventStruct.h
+++ Headers/EventStruct.h
@@ -20,4 +20,32 @@ typedef struct EventStruct {
     unsigned int mTaTime;
     double mWTaTime;
 } Event;
+
+void PrintEvent(const Event *pEvent) { //print an event using the same output file format
+    printf("At time %d ", pEvent->mTimeStep);
+    printf("process %d ", pEvent->mpProcess->mId);
+    switch (pEvent->mType) {
+        case START:
+            printf("started ");
+            break;
+        case STOP:
+            printf("stopped ");
+            break;
+        case CONT:
+            printf("resumed ");
+            break;
+        case FINISH:
+            printf("finished ");
+            break;
+        default:
+            printf("error ");
+            break;
+    }
+    printf("arr %d total %d ", pEvent->mpProcess->mArrivalTime, pEvent->mpProcess->mRuntime);
+    printf("remain %d wait %d", pEvent->mCurrentRemTime, pEvent->mCurrentWaitTime);
+    if (pEvent->mType == FINISH)
+        printf(" TA %d WTA %.2f", pEvent->mTaTime, pEvent->mWTaTime);
+    printf("\n");
+}
+
 #endif //OS_STARTER_CODE_EVENTSTRUCT_H
