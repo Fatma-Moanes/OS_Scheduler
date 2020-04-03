4c61777d7a075afbbfdec3a34bde59f089b1056e
shaffei <s-mohamed_chaffei@zewailcity.edu.eg>
2020-04-03 04:08:09 +0200

    Added save event to file function

--- Headers/EventStruct.h
+++ Headers/EventStruct.h
@@ -48,4 +48,31 @@ void PrintEvent(const Event *pEvent) { //print an event using the same output fi
     printf("\n");
 }
 
+void OutputEvent(const Event *pEvent, FILE *pFile) { //print an event using the same output file format
+    fprintf(pFile, "At time %d ", pEvent->mTimeStep);
+    fprintf(pFile, "process %d ", pEvent->mpProcess->mId);
+    switch (pEvent->mType) {
+        case START:
+            fprintf(pFile, "started ");
+            break;
+        case STOP:
+            fprintf(pFile, "stopped ");
+            break;
+        case CONT:
+            fprintf(pFile, "resumed ");
+            break;
+        case FINISH:
+            fprintf(pFile, "finished ");
+            break;
+        default:
+            fprintf(pFile, "error ");
+            break;
+    }
+    fprintf(pFile, "arr %d total %d ", pEvent->mpProcess->mArrivalTime, pEvent->mpProcess->mRuntime);
+    fprintf(pFile, "remain %d wait %d", pEvent->mCurrentRemTime, pEvent->mCurrentWaitTime);
+    if (pEvent->mType == FINISH)
+        fprintf(pFile, " TA %d WTA %.2f", pEvent->mTaTime, pEvent->mWTaTime);
+    fprintf(pFile, "\n");
+}
+
 #endif //OS_STARTER_CODE_EVENTSTRUCT_H
